9a2190c69585a39bb89437ce35acc1443f602759
Gavenda <gavenda@disroot.org>
2024-03-03 07:05:08 +0800

    Add clear scripts

---
+++ src/clear-dev.ts
@@ -0,0 +1,32 @@
+import { REST, Routes } from 'discord.js';
+import { logger } from './logger.js';
+
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
+
+if (!process.env.TOKEN) {
+  throw new Error('TOKEN is required.');
+}
+if (!process.env.CLIENT_ID) {
+  throw new Error('CLIENT_ID is required.');
+}
+if (!process.env.GUILD_ID) {
+  throw new Error('GUILD_ID is required.');
+}
+
+const rest = new REST().setToken(process.env.TOKEN);
+
+try {
+  const clientId = process.env.CLIENT_ID;
+  const guildId = process.env.GUILD_ID;
+
+  logger.info(`Started clearing application (/) commands on guild id: ${guildId}.`);
+
+  await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] });
+
+  logger.info(`Successfully cleared application (/) commands on guild id: ${guildId}.`);
+} catch (error) {
+  logger.error(error);
+}
---
+++ src/clear.ts
@@ -0,0 +1,28 @@
+import { REST, Routes } from 'discord.js';
+import { logger } from './logger.js';
+
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
+
+if (!process.env.TOKEN) {
+  throw new Error('TOKEN is required.');
+}
+if (!process.env.CLIENT_ID) {
+  throw new Error('CLIENT_ID is required.');
+}
+
+const rest = new REST().setToken(process.env.TOKEN);
+
+try {
+  const clientId = process.env.CLIENT_ID;
+
+  logger.info(`Started clearing application (/) commands.`);
+
+  await rest.put(Routes.applicationCommands(clientId), { body: [] });
+
+  logger.info(`Successfully cleared application (/) commands.`);
+} catch (error) {
+  logger.error(error);
+}
