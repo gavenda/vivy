e81122ad83eeaae3667b2174201df50d3928ca3c
Enda <gavenda@disroot.org>
2024-03-04 01:08:35 +0800

    1.0.1 (#2)
    * Add legacy players removal on ready
    
    * 1.0.1

--- src/events/ready.event.ts
+++ src/events/ready.event.ts
@@ -5,10 +5,27 @@ import { logger } from '@/logger';
 export const readyEvent: AppEvent<Events.ClientReady> = {
   event: Events.ClientReady,
   once: true,
-  execute: async ({ moon }, client) => {
+  execute: async ({ moon, redis }, client) => {
     logger.info(`Ready! Logged in`, { user: client.user.tag });
 
     // Init moon
     await moon.init(client.user.id);
+
+    // Cleanup legacy players
+    const legacyPlayers = await redis.sMembers(`player:legacy`);
+
+    for (const legacyPlayer of legacyPlayers) {
+      const [legacyChannelId, legacyMessageId] = legacyPlayer.split(':');
+      const legacyChannel = await client.channels.fetch(legacyChannelId);
+
+      if (legacyChannel?.isTextBased()) {
+        const legacyMessage = await legacyChannel.messages.fetch(legacyMessageId);
+
+        if (legacyMessage.author.id === client?.user?.id) {
+          logger.debug('Removing legacy message', { legacyMessageId });
+          await legacyMessage.delete();
+        }
+      }
+    }
   }
 };
