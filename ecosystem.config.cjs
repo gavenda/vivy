77b2f450f60b64ddac970d3897d58593bd59b94c
Gavenda <gavenda@disroot.org>
2024-03-04 02:35:07 +0800

    Add deployment setup script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -15,7 +15,16 @@ module.exports = {
       ref: 'origin/main',
       repo: 'https://github.com/gavenda/vivy',
       path: process.env.APP_PATH,
-      'post-deploy': 'pnpm install && pm2 reload ecosystem.config.js && pm2 save'
+      'post-deploy': 'pnpm install && pm2 reload ecosystem.config.js && pm2 save',
+      env: {
+        APP_ENV: 'production',
+        TOKEN: process.env.TOKEN,
+        CLIENT_ID: process.env.CLIENT_ID,
+        GUILD_ID: process.env.GUILD_ID,
+        SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+        SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+        REDIS_URL: process.env.REDIS_URL
+      }
     }
   }
 };
