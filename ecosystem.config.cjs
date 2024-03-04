a3e31fffe591eb4c288450ab907b04f64cccd550
Gavenda <gavenda@disroot.org>
2024-03-05 03:07:59 +0800

    Update ecosystem.config.cjs [skip ci]

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,3 +1,20 @@
+const deploymentConfig = (environment) => {
+  return {
+    user: process.env.APP_USER,
+    host: process.env.APP_HOST,
+    key: 'deploy.key',
+    ref: process.env.APP_BRANCH,
+    repo: 'https://github.com/gavenda/vivy',
+    path: process.env.APP_PATH,
+    // prettier-ignore
+    'post-deploy': 'pnpm install && pnpm run register && pm2 startOrRestart ecosystem.config.cjs && pm2 save',
+    env: {
+      APP_ENV: environment,
+      DOTENV_KEY: process.env.DOTENV_KEY
+    }
+  };
+};
+
 module.exports = {
   apps: [
     {
@@ -9,33 +26,7 @@ module.exports = {
     }
   ],
   deploy: {
-    production: {
-      user: process.env.APP_USER,
-      host: process.env.APP_HOST,
-      key: 'deploy.key',
-      ref: process.env.APP_BRANCH,
-      repo: 'https://github.com/gavenda/vivy',
-      path: process.env.APP_PATH,
-      'post-deploy':
-        'pnpm install && pnpm run register && pm2 startOrRestart ecosystem.config.cjs && pm2 save',
-      env: {
-        APP_ENV: 'production',
-        DOTENV_KEY: process.env.DOTENV_KEY
-      }
-    },
-    development: {
-      user: process.env.APP_USER,
-      host: process.env.APP_HOST,
-      key: 'deploy.key',
-      ref: process.env.APP_BRANCH,
-      repo: 'https://github.com/gavenda/vivy',
-      path: process.env.APP_PATH,
-      'post-deploy':
-        'pnpm install && pnpm run register && pm2 startOrRestart ecosystem.config.cjs && pm2 save',
-      env: {
-        APP_ENV: 'development',
-        DOTENV_KEY: process.env.DOTENV_KEY
-      }
-    }
+    production: deploymentConfig('production'),
+    development: deploymentConfig('development')
   }
 };
